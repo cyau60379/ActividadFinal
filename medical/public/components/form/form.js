549f768eb1d435153ef8b8e374e04081107bb9c2
cyau60379 <cyril.aubourg@isep.fr>
2021-04-25 15:08:45 +0200

    improve connection form

--- medical/public/components/form/form.js
+++ medical/public/components/form/form.js
@@ -4,24 +4,46 @@ function formController($scope, $element, $attrs) {
     ctrl.inputs = [];
     ctrl.changeForm = false;
 
-    ctrl.$onInit = function () {
-        if (ctrl.ftype == "connection") {
-            ctrl.inputs.push({name: "email", type: "email"});
-            ctrl.inputs.push({name: "password", type: "password"});
-            ctrl.changeForm = true;
-        } else if (ctrl.ftype == "inscription") {
-            ctrl.inputs.push({name: "name", type: "text"});
-            ctrl.inputs.push({name: "surname", type: "text"});
-            ctrl.inputs.push({name: "age", type: "number"});
-            ctrl.inputs.push({name: "address", type: "text"});
-            ctrl.inputs.push({name: "city", type: "text"});
-        } else {
-            // TODO: add an object in bindings with the name and the type
+    ctrl.fillInputs = function (type) {
+        switch (type) {
+            case "connection":
+                ctrl.inputs.push({name: "email", type: "email"});
+                ctrl.inputs.push({name: "password", type: "password"});
+                ctrl.changeForm = true;
+                ctrl.button = "sign up";
+                break;
+            case "inscription":
+                ctrl.inputs.push({name: "name", type: "text"});
+                ctrl.inputs.push({name: "surname", type: "text"});
+                ctrl.inputs.push({name: "age", type: "number"});
+                ctrl.inputs.push({name: "address", type: "text"});
+                ctrl.inputs.push({name: "city", type: "text"});
+                ctrl.changeForm = false;
+                ctrl.button = "sign in";
+                break;
+            default:
+                break;
         }
+        ctrl.ftype = type;
     }
 
     ctrl.sendData = function (sender) {
+        alert(ctrl.changeForm);
         // TODO: Use the chosen function
+    }
+
+    ctrl.change = function () {
+        ctrl.inputs = [];
+        if (ctrl.changeForm) {
+            ctrl.ftype = "inscription";
+        } else {
+            ctrl.ftype = "connection";
+        }
+        ctrl.fillInputs(ctrl.ftype);
+    }
+
+    ctrl.$onInit = function () {
+        ctrl.fillInputs(ctrl.ftype);
     }
 }
 
