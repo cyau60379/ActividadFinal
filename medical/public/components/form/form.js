4f059f9b8ae3b4f034dbb91c824c550b12d06b24
cyau60379 <cyril.aubourg@isep.fr>
2021-04-25 16:25:35 +0200

    improve connection

--- medical/public/components/form/form.js
+++ medical/public/components/form/form.js
@@ -3,16 +3,22 @@ function formController($scope, $element, $attrs) {
 
     ctrl.inputs = [];
     ctrl.changeForm = false;
+    $scope.datos = {};
+    $scope.enabled = false;
+
 
     ctrl.fillInputs = function (type) {
         switch (type) {
             case "connection":
-                ctrl.inputs.push({name: "email", type: "email"});
+                ctrl.inputs.push({name: "email", type: "text"});
                 ctrl.inputs.push({name: "password", type: "password"});
                 ctrl.changeForm = true;
                 ctrl.button = "sign up";
                 break;
             case "inscription":
+                ctrl.inputs.push({name: "email", type: "text"});
+                ctrl.inputs.push({name: "password", type: "password"});
+                ctrl.inputs.push({name: "confirmation", type: "password"});
                 ctrl.inputs.push({name: "name", type: "text"});
                 ctrl.inputs.push({name: "surname", type: "text"});
                 ctrl.inputs.push({name: "age", type: "number"});
@@ -24,12 +30,47 @@ function formController($scope, $element, $attrs) {
             default:
                 break;
         }
+        $scope.enabled = false;
         ctrl.ftype = type;
     }
 
+    ctrl.enableButton = function () {
+        var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        switch (ctrl.ftype) {
+            case "connection":
+                if (typeof $scope.datos.password == 'undefined' || $scope.datos.password.length < 8) {
+                    $scope.enabled = false;
+                } else if ($scope.datos.password.length >= 8) {
+                    $scope.enabled = $scope.datos.email.match(mailformat);
+                }
+                break;
+            case "inscription":
+                if (typeof $scope.datos.name == 'undefined'
+                    || typeof $scope.datos.surname == 'undefined'
+                    || typeof $scope.datos.age == 'undefined'
+                    || typeof $scope.datos.address == 'undefined'
+                    || typeof $scope.datos.city == 'undefined'
+                    || typeof $scope.datos.password == 'undefined'
+                    || typeof $scope.datos.confirmation == 'undefined'
+                    || typeof $scope.datos.email == 'undefined'
+                    || $scope.datos.password.length < 8
+                    || $scope.datos.password !== $scope.datos.confirmation) {
+                    $scope.enabled = false;
+                } else $scope.enabled = $scope.datos.email.match(mailformat);
+                break;
+        }
+    }
+
     ctrl.sendData = function (sender) {
-        alert(ctrl.changeForm);
-        // TODO: Use the chosen function
+        switch (ctrl.ftype) {
+            case "connection":
+                let email = $scope.datos.email;
+                let password = $scope.datos.password;
+                // TODO: use $http to send to the verification function + change page
+                break;
+            case "inscription":
+                // TODO: use $http to register the new patient + change page
+        }
     }
 
     ctrl.change = function () {
